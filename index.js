f36cb48560a0a504c7f21e97656d5a97e91ac2be
mesh <meshack.korir@student.moringaschool.com>
2023-03-20 00:31:06 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+const employee =  {
+    firstName: 'Emmanuel',
+    lastName: 'Kipsang',
+    role: 'Software Engineer',
+    payPerHour: 35,
+    timeInHours: 160,
+    hiredDate: new Date('2022-08-01'),
+    email: 'emmanuel@example.com',
+    phoneNumber: '555-555-5555'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+    return newEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
